478f545d64fb82d652bac5aec54473b36e107acd
Felipe Tribaldos <ef28db7dcf6ff17048d8@cloudflare.com>
2022-09-19 06:28:57 +0000

    Send rule

--- tool/mypdns/reporter/addon/firefox/conf.js
+++ tool/mypdns/reporter/addon/firefox/conf.js
@@ -36,6 +36,7 @@ document.addEventListener('DOMContentLoaded', () => {
       document.getElementById('nocat19').checked = (r.nocat19 == '1') ? true : false;
       document.getElementById('nocat20').checked = (r.nocat20 == '1') ? true : false;
       document.getElementById('nocat21').checked = (r.nocat21 == '1') ? true : false;
+      document.getElementById('nocat22').checked = (r.nocat22 == '1') ? true : false;
       document.getElementById('nocat86').checked = (r.nocat86 == '1') ? true : false;
       document.getElementById('nocat87').checked = (r.nocat87 == '1') ? true : false;
       document.getElementById('nocat88').checked = (r.nocat88 == '1') ? true : false;
